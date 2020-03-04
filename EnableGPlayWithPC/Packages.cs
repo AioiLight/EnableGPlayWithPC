982bdc0b07c43c60b433caa0ac9d5b32ea3f5720
AioiLight <info@aioilight.space>
2020-03-04 14:56:51 +0900

    プロジェクト ファイルを追加します。

---
+++ EnableGPlayWithPC/Packages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnableGPlayWithPC
+{
+    internal class Packages
+    {
+        internal string Vending = "com.android.vending";
+        internal string GMS = "com.google.android.gms";
+        internal string GSF = "com.google.android.gsf";
+        internal string GSFLogin = "com.google.android.gsf.login";
+    }
+}
