d57be44f1d6ae4b199403f01abae8f36a9ec4592
AioiLight <info@aioilight.space>
2020-03-04 16:50:35 +0900

    エラーダイアログとusing整理

---
+++ EnableGPlayWithPC/ErrorDialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace EnableGPlayWithPC
+{
+    internal static class ErrorDialog
+    {
+        internal static void ShowError(string instruction, string desc, IntPtr handle)
+        {
+            var dialog = new TaskDialog();
+            dialog.Caption = "Enable GPlay With PC";
+            dialog.InstructionText = instruction;
+            dialog.Text = desc;
+            dialog.Icon = TaskDialogStandardIcon.Error;
+            dialog.OwnerWindowHandle = handle;
+            dialog.Show();
+        }
+    }
+}
--- EnableGPlayWithPC/Main.cs
+++ EnableGPlayWithPC/Main.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
 using SharpAdbClient;
 using SharpAdbClient.DeviceCommands;
 
@@ -30,47 +26,88 @@ namespace EnableGPlayWithPC
 
         private void Button_Process_Click(object sender, EventArgs e)
         {
+            foreach (var path in GetSelectedPath())
+            {
+                if (!File.Exists(path))
+                {
+                    ErrorDialog.ShowError($"{path} is not found",
+                        $"{path} is not found.\n" +
+                        "Please check to exist that file.", this.Handle);
+                    return;
+                }
+            }
+
+
             var appDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
             var adb = new AdbServer();
 
-            var result = adb.StartServer(Path.Combine(appDir, Properties.Resources.AdbPath), true);
-
-            var device = AdbClient.Instance.GetDevices().First();
-
-            var packageManager = new PackageManager(device);
-
-            // それぞれアンインストール
-            Array.ForEach(Packages.PackageNames, pkg => packageManager.UninstallPackage(pkg));
-
-            // パスを取得
-            var apks = GetSelectedPath();
-            // それぞれインストール
-            Array.ForEach(apks, apk => packageManager.InstallPackage(apk, true));
-
-            // Play ストアに権限付与
-            foreach (var perm in Permissions.Vending)
+            try
             {
-                var cmd = $"pm grant {Packages.Vending} {Permissions.Prefix}{perm}";
-                AdbClient.Instance.ExecuteRemoteCommand(cmd, device, null);
+                var result = adb.StartServer(Path.Combine(appDir, Properties.Resources.AdbPath), true);
+            }
+            catch (Exception)
+            {
+                ErrorDialog.ShowError("adb.exe is not found", "adb.exe is not found.\n" +
+                            "Please check adb.exe's path.", this.Handle);
+                return;
             }
 
-            // GooglePlay開発者サービスはに権限付与
-            foreach (var perm in Permissions.GMS)
+            try
             {
-                var cmd = $"pm grant {Packages.GMS} {Permissions.Prefix}{perm}";
-                AdbClient.Instance.ExecuteRemoteCommand(cmd, device, null);
+                var device = AdbClient.Instance.GetDevices().First();
+
+                if (AdbClient.Instance.GetDevices().Count > 1)
+                {
+                    ErrorDialog.ShowError("Too many connected devices",
+                        "Too many connected devices.\n" +
+                        "Please connect only one devices.", this.Handle);
+                    return;
+                }
+
+                var packageManager = new PackageManager(device);
+
+                // それぞれアンインストール
+                Array.ForEach(Packages.PackageNames, pkg => packageManager.UninstallPackage(pkg));
+
+                // パスを取得
+                var apks = GetSelectedPath();
+                // それぞれインストール
+                Array.ForEach(apks, apk => packageManager.InstallPackage(apk, true));
+
+                // Play ストアに権限付与
+                foreach (var perm in Permissions.Vending)
+                {
+                    var cmd = $"pm grant {Packages.Vending} {Permissions.Prefix}{perm}";
+                    AdbClient.Instance.ExecuteRemoteCommand(cmd, device, null);
+                }
+
+                // GooglePlay開発者サービスはに権限付与
+                foreach (var perm in Permissions.GMS)
+                {
+                    var cmd = $"pm grant {Packages.GMS} {Permissions.Prefix}{perm}";
+                    AdbClient.Instance.ExecuteRemoteCommand(cmd, device, null);
+                }
             }
+            catch (Exception)
+            {
+                ErrorDialog.ShowError("Unable to connect your device",
+                    "Unable to connect your device.\n" +
+                    "Please check connection.", this.Handle);
+                return;
+            }
+
+            var dialog = new TaskDialog();
         }
 
         private string[] GetSelectedPath()
         {
             return new string[]
             {
-                FileSelector_Vending.Text,
-                FileSelector_GMS.Text,
-                FileSelector_GSF.Text,
-                FileSelector_GSFLogin.Text
+                FileSelector_Vending.GetPath(),
+                FileSelector_GMS.GetPath(),
+                FileSelector_GSF.GetPath(),
+                FileSelector_GSFLogin.GetPath()
             };
         }
     }
