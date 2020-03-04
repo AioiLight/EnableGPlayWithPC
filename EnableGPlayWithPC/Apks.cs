3c2653eb7cecf82a00cdeda6e6c44a7ddc3ccde0
AioiLight <info@aioilight.space>
2020-03-04 15:44:20 +0900

    初期化処理

---
+++ EnableGPlayWithPC/Apks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnableGPlayWithPC
+{
+    internal static class Apks
+    {
+        internal static string Vending = "GooglePlayStore.apk";
+        internal static string GMS = "GooglePlayServices.apk";
+        internal static string GSF = "GoogleServicesFramework.apk";
+        internal static string GSFLogin = "GoogleLoginService.apk";
+    }
+}
--- EnableGPlayWithPC/Main.cs
+++ EnableGPlayWithPC/Main.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 
@@ -14,6 +16,14 @@ namespace EnableGPlayWithPC
         public Main()
         {
             InitializeComponent();
+
+            var appDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // それぞれのTextBoxにデフォルトのパスを入れておく
+            FileSelector_Vending.Init(Path.Combine(appDir, Apks.Vending));
+            FileSelector_GMS.Init(Path.Combine(appDir, Apks.GMS));
+            FileSelector_GSF.Init(Path.Combine(appDir, Apks.GSF));
+            FileSelector_GSFLogin.Init(Path.Combine(appDir, Apks.GSFLogin));
         }
     }
 }
