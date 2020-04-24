ef842523bd544111f3f95dd866c224703d5656fe
AioiLight <info@aioilight.space>
2020-04-24 15:40:13 +0900

    GMSに必要な権限の変更

--- EnableGPlayWithPC/Permissions.cs
+++ EnableGPlayWithPC/Permissions.cs
@@ -15,24 +15,10 @@
 
         internal static string[] GMS = new string[]
         {
-            "DUMP",
             "READ_LOGS",
             "WRITE_SECURE_SETTINGS",
             "INTERACT_ACROSS_USERS",
-            "CHANGE_WIMAX_STATE",
-            "CLEAR_APP_CACHE",
-            "CLEAR_APP_USER_DATA",
-            "CONFIGURE_WIFI_DISPLAY",
-            "CONNECTIVITY_INTERNAL",
-            "CONTROL_KEYGUARD",
-            "CONTROL_LOCATION_UPDATES",
-            "CONTROL_WIFI_DISPLAY",
-            "COPY_PROTECTED_DATA",
-            "CREATE_USERS",
-            "CRYPT_KEEPER",
-            "DELETE_CACHE_FILES",
-            "DELETE_PACKAGES",
-            "DEVICE_POWER"
+            "GET_APP_OPS_STATS"
         };
     }
 }
